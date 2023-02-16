408b6f268621bc108ff1d13da809d7ec8e499fd3
ercx <ercx@fresnou.org>
2023-02-16 10:29:04 -0800

    Chapter 2 notes

---
+++ inputs2.js
@@ -0,0 +1,30 @@
+const readLine = require('readline-sync');
+
+let number = Number(readLine.question("give a number for the sum:"));
+let other = Number(readLine.question("give the next for the sum:"));
+let div = number + other;
+  console.log(`sum: ${div}`);
+
+let number1 = Number(readLine.question("give a number for the difference:"));
+let other1 = Number(readLine.question("give the next for the difference:"));
+let div1 = number1 - other1;
+console.log(`difference: ${div1}`);
+
+let number2 = Number(readLine.question("give a number for the sum:"));
+let other2 = Number(readLine.question("give the next for the sum:"));
+let div2 = number2 + other2;
+console.log(`sum: ${div2}`);
+
+let number3 = Number(readLine.question("give a number for the sum:"));
+let other3 = Number(readLine.question("give the next for the sum:"));
+let div3 = number3 + other3;
+console.log(`sum: ${div3}`);
+
+let number4 = Number(readLine.question("give a number for the sum:"));
+let other4 = Number(readLine.question("give the next for the sum:"));
+let div4 = number4 + other4;
+console.log(`sum: ${div4}`);
+
+
+
+
