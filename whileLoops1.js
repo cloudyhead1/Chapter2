45f8a370ecbaa594157926bd93ed47c1505fa106
ercx <ercx@fresnou.org>
2023-02-17 10:27:21 -0800

    Chapter 2 notes

---
+++ whileLoops1.js
@@ -0,0 +1,25 @@
+const readLine = require('readline-sync');
+
+let x = 0;
+while (x<26){
+    console.log(x);
+    x++;
+}
+
+let y = 0;
+while (y<51){
+    console.log(y);
+    y++;
+}
+
+let z = 11;
+while (z<-10){
+    console.log(z);
+    z++;
+}
+
+let v = 0;
+while (v>-51){
+    console.log(v);
+    v--;
+}
